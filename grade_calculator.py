2331d2ce4c3d70853d76afaba34268e7f7574717
pinidi-siriwardana <siriwardana.pinidi@gmail.com>
2026-03-06 13:54:13 +0530

    added exit to the file

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,22 +1,32 @@
 # grade_calculator.py
 
-# Ask for student's name
-name = input("Enter the student's name: ")
+while True:
+    name = input("Enter student's name (or type 'Exit' to stop): ")
 
-# Ask for 3 subject marks
-mark1 = float(input("Enter mark for subject 1: "))
-mark2 = float(input("Enter mark for subject 2: "))
-mark3 = float(input("Enter mark for subject 3: "))
+    if name.lower() == "exit":
+        print("Program ended.")
+        break
 
-# Calculate average
-average = (mark1 + mark2 + mark3) / 3
+    # Ask for 3 subject marks
+    mark1 = float(input("Enter mark for subject 1: "))
+    mark2 = float(input("Enter mark for subject 2: "))
+    mark3 = float(input("Enter mark for subject 3: "))
 
-# Display average
-print("\nStudent Name:", name)
-print("Average Marks:", average)
+    # Calculate average
+    average = (mark1 + mark2 + mark3) / 3
 
-# Check pass or fail
-if average >= 40:
-    print("Result: Pass")
-else:
-    print("Result: Fail")
+    print("\nStudent Name:", name)
+    print("Average Marks:", average)
+
+    # Assign grade
+    if average >= 75:
+        grade = "A"
+    elif average >= 60:
+        grade = "B"
+    elif average >= 40:
+        grade = "C"
+    else:
+        grade = "Fail"
+
+    print("Grade:", grade)
+    print("------------------------")
